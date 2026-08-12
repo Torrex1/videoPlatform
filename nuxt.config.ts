553307ea401bb2459c443f1ff61c970e94f48441
Torrex1 <flendik666@gmail.com>
2026-08-13 02:18:23 +0300

    init

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  srcDir: 'src/',
+  css: [
+    '@/app/styles/main.css'
+  ],
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true }
 })
