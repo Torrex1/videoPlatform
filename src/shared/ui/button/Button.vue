dd70b9aadd70886627f04267a0d0976ae3819a41
Torrex1 <flendik666@gmail.com>
2026-09-14 00:22:39 +0300

    create new shared's elements

---
+++ src/shared/ui/button/Button.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+const props = defineProps<{
+    text?: string,
+}>()
+</script>
+
+<template>
+    <button>
+        <span>{{ text }}</span>
+    </button>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/shared/ui/sidebar/SidebarItem.vue
@@ -0,0 +1,38 @@
+<script lang="ts" setup >
+defineProps<{
+    itemText: string;
+    linkTo?: string;
+}>();
+</script>
+
+<template>
+    <li class="sidebar__list-item sidebar__item">
+        <img class="sidebar__item-icon" src="../../../assets/search.svg" alt="itemIcon">
+        <a class="sidebar__item-link" :href="linkTo">{{ itemText }}</a>
+    </li>
+</template>
+
+<style scoped>
+    .sidebar__list-item {
+        display: flex;
+        gap: 10px;
+
+        padding: 20px;
+        border: 1px solid #e4e4e4ab;
+        border-radius: 10px;
+        cursor: pointer;
+    }
+    .sidebar__item-icon {
+        width: 16px;
+    }
+    .sidebar__item-link {
+        font-size: 18px;
+    }
+
+    .sidebar__list-item:hover {
+        box-shadow: 2px 5px 8px 1px #d8d8d865;
+    }
+    .sidebar__list-item:active {
+        box-shadow: unset;
+    }
+</style>
